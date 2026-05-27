d05c2e7c480e0e697478fd3b4b8b3c5d56c5e251
Andrew Godinez Barahona <andrewgb20@protonmail.com>
2026-05-26 23:26:24 -0600

    Añadidos todos los paquetes
    Queda pendiente el refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module una.ac.cr.kingdomfantasy {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
+    requires jakarta.persistence;
 
     opens una.ac.cr.kingdomfantasy to javafx.fxml;
     exports una.ac.cr.kingdomfantasy;
---
+++ src/main/java/una/ac/cr/kingdomfantasy/model/Object.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package una.ac.cr.kingdomfantasy.model;
+
+/**
+ *
+ * @author andrew
+ */
+public interface Object {
+    
+    public abstract int ready();
+    
+    public abstract int update();
+    
+    public abstract void draw();
+    
+}
