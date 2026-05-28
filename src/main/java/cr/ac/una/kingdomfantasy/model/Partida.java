b319be29436125e1927a2c7706a6e0a0cab37cdd
AndresCortesVictor <andres.cortes.victor@est.una.ac.cr>
2026-05-28 13:38:48 -0600

    Conection from database done and entities creaded

---
+++ src/main/java/cr/ac/una/kingdomfantasy/model/Partida.java
@@ -0,0 +1,160 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.kingdomfantasy.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Version;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "DEF_PARTIDA")
+@NamedQueries({
+    @NamedQuery(name = "Partida.findAll", query = "SELECT p FROM Partida p"),
+    @NamedQuery(name = "Partida.findByParId", query = "SELECT p FROM Partida p WHERE p.parId = :parId"),
+    })
+public class Partida implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @SequenceGenerator(name = "DEF_PARTIDA_PAR_ID_GENERATOR", sequenceName = "una.DEF_PARTIDA_SEQ01", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DEF_PARTIDA_PAR_ID_GENERATOR")
+    @Basic(optional = false)
+    @Column(name = "PAR_ID")
+    private Long id;
+    @Column(name = "PAR_NIVEL_ACTUAL")
+    private Integer nivelActual;
+    @Column(name = "PAR_PUNTOS_ACTUALES")
+    private Long puntosActuales;
+    @Column(name = "PAR_FECHA_GUARDADO")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDate fechaGuardado;
+    @Basic(optional = false)
+    @Column(name = "PAR_VERSION")
+    @Version
+    private Long version;
+    @JoinColumn(name = "PAR_IDMEJ", referencedColumnName = "MEJ_ID")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Mejora idmej;
+    @JoinColumn(name = "PAR_IDPLY", referencedColumnName = "PLY_ID")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Player idply;
+
+    public Partida() {
+    }
+
+    public Partida(Long parId) {
+        this.id = parId;
+    }
+
+    public Partida(Long parId, Long parVersion) {
+        this.id = parId;
+        this.version = parVersion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNivelActual() {
+        return nivelActual;
+    }
+
+    public void setNivelActual(Integer nivelActual) {
+        this.nivelActual = nivelActual;
+    }
+
+    public Long getPuntosActuales() {
+        return puntosActuales;
+    }
+
+    public void setPuntosActuales(Long puntosActuales) {
+        this.puntosActuales = puntosActuales;
+    }
+
+    public LocalDate getFechaGuardado() {
+        return fechaGuardado;
+    }
+
+    public void setFechaGuardado(LocalDate fechaGuardado) {
+        this.fechaGuardado = fechaGuardado;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Mejora getIdmej() {
+        return idmej;
+    }
+
+    public void setIdmej(Mejora idmej) {
+        this.idmej = idmej;
+    }
+
+    public Player getIdply() {
+        return idply;
+    }
+
+    public void setIdply(Player idply) {
+        this.idply = idply;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Partida)) {
+            return false;
+        }
+        Partida other = (Partida) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cr.ac.una.kingdomfantasy.model.Partida[ parId=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/cr/ac/una/kingdomfantasy/model/Player.java
@@ -0,0 +1,175 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.kingdomfantasy.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "DEF_PLAYER")
+@NamedQueries({
+    @NamedQuery(name = "Player.findAll", query = "SELECT p FROM Player p"),
+    @NamedQuery(name = "Player.findByPlyId", query = "SELECT p FROM Player p WHERE p.plyId = :plyId")
+   })
+public class Player implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @SequenceGenerator(name = "DEF_JUGADOR_PLY_ID_GENERATOR", sequenceName = "una.DEF_PLAYER_SEQ01", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DEF_JUGADOR_PLY_ID_GENERATOR")
+    @Basic(optional = false)
+    @Column(name = "PLY_ID")
+    private Long id;
+    @Lob
+    @Column(name = "PLY_FOTO_PERFIL")
+    private Serializable fotoPerfil;
+    @Basic(optional = false)
+    @Column(name = "PLY_NOMBRE")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "PLY_FECHA_REGISTRO")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDate fechaRegistro;
+    @Basic(optional = false)
+    @Column(name = "PLY_VERSION")
+    private Long version;
+    @Column(name = "PLY_PUNTOS_TOTALES")
+    private Long puntosTotales;
+    @Basic(optional = false)
+    @Column(name = "PLY_ID_BALLESTA")
+    private Integer idBallesta;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "parIdply", fetch = FetchType.LAZY)
+    private List<Partida> partidaList;
+
+    public Player() {
+    }
+
+    public Player(Long plyId) {
+        this.id = plyId;
+    }
+
+//    public Player(BigDecimal plyId, String plyNombre, Date plyFechaRegistro, BigInteger plyVersion, BigInteger plyIdBallesta) {
+//        this.id = plyId;
+//        this.nombre = plyNombre;
+//        this.fechaRegistro = plyFechaRegistro;
+//        this.version = plyVersion;
+//        this.idBallesta = plyIdBallesta;
+//    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Serializable getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(Serializable fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Long getPuntosTotales() {
+        return puntosTotales;
+    }
+
+    public void setPuntosTotales(Long puntosTotales) {
+        this.puntosTotales = puntosTotales;
+    }
+
+    public Integer getIdBallesta() {
+        return idBallesta;
+    }
+
+    public void setIdBallesta(Integer idBallesta) {
+        this.idBallesta = idBallesta;
+    }
+
+    public List<Partida> getPartidaList() {
+        return partidaList;
+    }
+
+    public void setPartidaList(List<Partida> partidaList) {
+        this.partidaList = partidaList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Player)) {
+            return false;
+        }
+        Player other = (Player) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cr.ac.una.kingdomfantasy.model.Player[ plyId=" + id + " ]";
+    }
+    
+}
