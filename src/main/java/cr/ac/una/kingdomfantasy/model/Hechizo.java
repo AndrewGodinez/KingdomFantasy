2bc20ad13eb01df7f9446e5a4958fd31e81bb476
Andrew Godinez Barahona <andrewgb20@protonmail.com>
2026-05-31 21:24:58 -0600

    Ballesta, Hechizo y Proyectil

---
+++ src/main/java/cr/ac/una/kingdomfantasy/model/Hechizo.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.kingdomfantasy.model;
+
+/**
+ *
+ * @author andrew
+ */
+public class Hechizo extends Entidad implements HaceDano {
+    
+    private TipoHechizo tipoHechizo;
+    private Integer costoMana;
+    private Integer danio;
+    private Integer coolDownMax;
+    private Integer coolDownActual;
+    private Integer duracionEfecto;
+    private Integer radio;
+    private Boolean estado;
+
+    @Override
+    public Integer obtenerDanoActual() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void aplicarDano(Entidad entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void actualizar(Double deltaTime) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void manejarEvento(TipoEvento evento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/cr/ac/una/kingdomfantasy/model/TipoHechizo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package cr.ac.una.kingdomfantasy.model;
+
+/**
+ *
+ * @author andrew
+ */
+public enum TipoHechizo {
+    METEORO,
+    HIELO;
+}
