05a1d5fa8b1491212b9c17773a6de3f52abe8130
SemMora <semmora04@gmail.com>
2026-06-01 04:01:36 -0600

    Update SpriteAnimator.java

--- src/main/java/cr/ac/una/kingdomfantasy/util/SpriteAnimator.java
+++ src/main/java/cr/ac/una/kingdomfantasy/util/SpriteAnimator.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cr.ac.una.kingdomfantasy.util;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.ImageView;
 
 /**
  *
  * @author semmo
  */
 public class SpriteAnimator {
-    
+    private final ImageView imageView;
+    private final int frameWidth;
+    private final int frameHeight;
+    private final int totalFrames;
+    private final double frameDuration;
+
+    private int frameIndex = 0;
+    private double elapsed = 0;
+
+    public SpriteAnimator(ImageView imageView, int frameWidth, int frameHeight,
+                          int totalFrames, double frameDuration) {
+        this.imageView = imageView;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.totalFrames = totalFrames;
+        this.frameDuration = frameDuration;
+        actualizarViewport();
+    }
+
+    public void update(double deltaTime) {
+        elapsed += deltaTime;
+        if (elapsed >= frameDuration) {
+            elapsed = 0;
+            frameIndex = (frameIndex + 1) % totalFrames;
+            actualizarViewport();
+        }
+    }
+
+    private void actualizarViewport() {
+        imageView.setViewport(new Rectangle2D(
+            frameIndex * frameWidth, 0, frameWidth, frameHeight
+        ));
+    }
+
+    public void reiniciar() {
+        frameIndex = 0;
+        elapsed = 0;
+        actualizarViewport();
+    }
 }
